d40a0fcbbd882f832edd541a3a6537b307205cbc
Nicolás Malpic <nmalpicf@gmail.com>
2024-11-26 09:46:06 -0500

    Added Task module into admin panel

--- taskmanager/tasks/admin.py
+++ taskmanager/tasks/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'created_at')
+    list_filter = ('status',)
+    search_fields = ('title', 'description')
+
+admin.site.register(Task)
